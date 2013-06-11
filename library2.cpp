240bd5137c88413fa08ee22bb366b33e93be6f46
AviadRom <b2faviad@gmail.com>
2013-06-11 23:44:35 +0300

    Update library2.cpp

--- library2.cpp
+++ library2.cpp
@@ -1,32 +1,14 @@
-<<<<<<< HEAD
-/*library2.cpp
- *
- *  Created on: Jun 11, 2013
- *       Author: OHAD FIREMAN
- *
-=======
-/*
- * library2.cpp
- *
- *  Created on: Jun 11, 2013
- *      Author: OHAD FIREMAN
->>>>>>> 61f1edb7a7f8d88830a506652e20bc884e664ab9
- */
-
-
-
-
+//
+//  library2.cpp
+//  DS WET2
+//
+//  Aviad Rom and Ohad Fireman
+//
 #include "library2.h"
 #include "Tasker.h"
 
-
-<<<<<<< HEAD
 void* Init(int N, int *tasksIDs, int *priorities){
   Tasker* DS = new Tasker(N, tasksIDs, priorities);
-=======
-void* Init(){
-  Tasker* DS = new Tasker();
->>>>>>> 61f1edb7a7f8d88830a506652e20bc884e664ab9
 	return (void*)DS;
 }
 
@@ -54,17 +36,8 @@ StatusType GetKthTask(void* DS,int k,int* taskID,int* priority){
 	return ((Tasker *)DS)->GetKthTask(k,taskID,priority);
 }
 
-<<<<<<< HEAD
 void Quit(void** DS){
 	Tasker* tmp = (Tasker*)*DS;
 	delete tmp;
 	DS = NULL;
 }
-
-
-=======
-StatusType Quit(void** DS){
-	return ((Tasker *)DS)->Quit();
-}
-
->>>>>>> 61f1edb7a7f8d88830a506652e20bc884e664ab9
